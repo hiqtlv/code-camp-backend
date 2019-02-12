44c30b8aac3003a24b7f2680121e79cdeeff87b9
Ernesto Garcia <Ernesto.Garcia@hiq.se>
2019-02-12 11:21:01 +0100

    Added pmd, checkstyle, jacoco

--- src/main/java/se/hiq/ping/PingController.java
+++ src/main/java/se/hiq/ping/PingController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PingController {
 
-    @GetMapping("/ping")
-    public String ping() {
-        return "OK";
-    }
+  @GetMapping("/ping")
+  public String ping() {
+    return "OK";
+  }
 
 }
